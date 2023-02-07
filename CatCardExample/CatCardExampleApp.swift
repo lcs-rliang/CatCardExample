1e0a58520e3ee796f5e941a8949c4a399a36ef89
Ruiyan Liang <Jliang@lcs.on.ca>
2023-02-07 09:07:42 -0500

    Initial Commit

---
+++ CatCardExample/CatCardExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatCardExampleApp.swift
+//  CatCardExample
+//
+//  Created by dyhidrogen monoxide on 2023/2/7.
+//
+
+import SwiftUI
+
+@main
+struct CatCardExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
